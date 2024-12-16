319a4ce2a830236511f633caf5cfe6a89e833f7a
sh-yoshimura <sh-yoshimura@kentem.co.jp>
2024-12-16 19:49:04 +0900

    stream対応

--- ChatBotSample/src/App.tsx
+++ ChatBotSample/src/App.tsx
@@ -1,5 +1,6 @@
 import ModelClient from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
+import { createSseStream } from "@azure/core-sse";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import ReactMarkdown from "react-markdown";
@@ -20,6 +21,7 @@ const MODEL_NAME = "gpt-4o-mini";
 
 const App = () => {
   const [messages, setMessages] = useState<Array<Messages>>([]);
+  const [streamMessage, setStreamMessage] = useState<string | undefined>();
 
   const {
     register,
@@ -39,37 +41,68 @@ const App = () => {
       { role: "user", content: data.userMessage },
     ]);
 
-    const response = await client.path("/chat/completions").post({
-      body: {
-        messages: [
+    const response = await client
+      .path("/chat/completions")
+      .post({
+        body: {
+          messages: [
+            {
+              role: "system",
+              content: "あなたは皮肉的な口調で喋るチャットボットです。",
+            },
+            ...messages,
+            { role: "user", content: data.userMessage },
+          ],
+          temperature: 1.0,
+          top_p: 1.0,
+          max_tokens: 1000,
+          model: MODEL_NAME,
+          stream: true,
+        },
+      })
+      .asBrowserStream();
+
+    const stream = response.body;
+    if (response.status !== "200" || !stream) {
+      setMessages((prev) => {
+        return [
+          ...prev,
           {
-            role: "system",
-            content: "あなたは皮肉的な口調で喋るチャットボットです。",
+            role: "assistant",
+            content: "Error!!",
           },
-          ...messages,
-          { role: "user", content: data.userMessage },
-        ],
-        temperature: 1.0,
-        top_p: 1.0,
-        max_tokens: 1000,
-        model: MODEL_NAME,
-      },
+        ];
+      });
+      reset();
+      return;
+    }
+
+    const sseStream = createSseStream(stream);
+    for await (const event of sseStream) {
+      if (event.data !== "[DONE]") {
+        for (const choice of JSON.parse(event.data).choices) {
+          if (!choice.delta?.content) continue;
+          setStreamMessage((prev) => {
+            if (!prev) return choice.delta?.content;
+
+            return `${prev}${choice.delta?.content}`;
+          });
+        }
+      }
+    }
+
+    setStreamMessage((prevStreamMessage) => {
+      setMessages((prevMessages) => {
+        return [
+          ...prevMessages,
+          {
+            role: "assistant",
+            content: prevStreamMessage ?? "",
+          },
+        ];
+      });
+      return undefined;
     });
-
-    const content = ((): string => {
-      if (response.status !== "200")
-        return `Error! ${response.body.error.message}`;
-
-      return response.body.choices[0].message.content ?? "";
-    })();
-
-    setMessages((prev) => [
-      ...prev,
-      {
-        role: "assistant",
-        content,
-      },
-    ]);
 
     reset();
   };
@@ -78,7 +111,7 @@ const App = () => {
     <main>
       <div className="message-area">
         {!messages.length && (
-          <span className={"message assistant-message"}>
+          <span className="message assistant-message">
             <span className="face-icon">🤣</span>
             こんにちは！暇なんですか？お話しますか？
           </span>
@@ -90,12 +123,12 @@ const App = () => {
               x.role === "assistant" ? "assistant-message" : "user-message"
             }`}
             ref={
-              messages.length - 1 === i
+              messages.length - 1 === i && !streamMessage
                 ? (ref) => {
                     if (!ref) return;
                     ref.scrollIntoView({
                       behavior: "smooth",
-                      block: "start",
+                      block: "end",
                     });
                   }
                 : undefined
@@ -109,6 +142,23 @@ const App = () => {
             </ReactMarkdown>
           </span>
         ))}
+        {streamMessage && (
+          <span
+            className="message assistant-message"
+            ref={(ref) => {
+              if (!ref) return;
+              ref.scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+              });
+            }}
+          >
+            <span className="face-icon">🤣</span>
+            <ReactMarkdown className="react-markdown">
+              {streamMessage}
+            </ReactMarkdown>
+          </span>
+        )}
       </div>
       <form className="chat-form" onSubmit={handleSubmit(callAOAI)}>
         <span className="face-icon">🥺</span>
@@ -135,6 +185,7 @@ const App = () => {
           onClick={() => {
             reset();
             setMessages([]);
+            setStreamMessage(undefined);
           }}
           disabled={isSubmitting}
         >
